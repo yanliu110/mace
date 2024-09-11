fac5234afbcff593378942da01cde033d6f7910c
刘焱 <99227383+yanliu110@users.noreply.github.com>
2024-09-11 11:05:56 +0800

    Update slurm_distributed.py

--- mace/tools/slurm_distributed.py
+++ mace/tools/slurm_distributed.py
@@ -33,8 +33,12 @@ class DistributedEnvironment:
         # os.environ["LOCAL_RANK"] = os.environ["SLURM_LOCALID"]
         # os.environ["RANK"] = os.environ["SLURM_PROCID"]
         
-        os.environ["MASTER_ADDR"] = "localhost"
-        os.environ["MASTER_PORT"] = "33333"
-        os.environ["WORLD_SIZE"] = "2"
-        os.environ["RANK"] = "0"  # 设置每个进程的 rank
-        os.environ["LOCAL_RANK"] = "0"  # 设置每个进程在本地的 rank
+       os.environ["MASTER_ADDR"] = "localhost"
+       os.environ["MASTER_PORT"] = "33333"
+       os.environ["WORLD_SIZE"] = "2"  # 设置你总共的进程数量
+       os.environ["RANK"] = os.environ.get("RANK", "0")  # 当前进程的 rank
+       os.environ["LOCAL_RANK"] = os.environ.get("LOCAL_RANK", "0")  # 本地的 rank
+       dist.init_process_group(backend="nccl", init_method="env://")
+       local_rank = int(os.environ["LOCAL_RANK"])
+       torch.cuda.set_device(local_rank)
+       print(f"Process {dist.get_rank()} is using GPU {local_rank}")
